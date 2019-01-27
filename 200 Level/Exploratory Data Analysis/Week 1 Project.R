c30740dc4b400bfdf8073737f7a2ac26859b52dd
Connor Voglewede <cvoglewede@redventures.com>
2019-01-26 19:58:08 -0500

    Updating Week 1 Coursera Exploratory Data Analysis Project

--- 200 Level/Exploratory Data Analysis/Week 1 Project.R
+++ 200 Level/Exploratory Data Analysis/Week 1 Project.R
@@ -43,6 +43,41 @@ epc2 <- epc_subset
 epc2$Global_active_power <- as.numeric(epc2$Global_active_power)
 epc2$DateTime <- lubridate::ymd_hms(paste(epc2$Date,epc2$Time))
 
-plot(epc2$DateTime,epc2$Global_active_power,xlab="Global Active Power (kilowatts)")
+plot(epc2$DateTime,epc2$Global_active_power,type="l",xlab=" ",ylab="Global Active Power (kilowatts)")
 
+# Plot 3
+
+epc3 <- epc_subset
+epc3$DateTime <- lubridate::ymd_hms(paste(epc2$Date,epc2$Time))
+
+
+plot(epc3$DateTime,epc3$Sub_metering_1,type="l",xlab=" ",ylab="Energy sub metering",col=1)
+lines(epc3$DateTime,epc3$Sub_metering_2,type="l",col=2)
+lines(epc3$DateTime,epc3$Sub_metering_3,type="l",col=4)
+legend("topright",legend=c(names(epc3)[7:9]),col=c(1,2,4),lty=1)
+
+
+# Plot 4
+
+epc4 <- epc_subset
+epc4$DateTime <- lubridate::ymd_hms(paste(epc2$Date,epc2$Time))
+
+par(mfrow=c(2,2))
+
+# upper left
+epc4$Global_active_power <- as.numeric(epc4$Global_active_power)
+plot(epc4$DateTime,epc4$Global_active_power,type="l",xlab=" ",ylab="Global Active Power (kilowatts)")
+
+# upper right
+plot(epc4$DateTime,epc4$Voltage,type="l",xlab="datetime",ylab="Voltage")
+
+
+# bottom left
+plot(epc4$DateTime,epc4$Sub_metering_1,type="l",xlab=" ",ylab="Energy sub metering",col=1)
+lines(epc4$DateTime,epc4$Sub_metering_2,type="l",col=2)
+lines(epc4$DateTime,epc4$Sub_metering_3,type="l",col=4)
+legend("topright",legend=c(names(epc4)[7:9]),col=c(1,2,4),bty="n",lty=1)
+
+# bottom right
+plot(epc4$DateTime,epc4$Global_reactive_power,type="l",xlab="datetime",ylab=names(epc4$Global_reactive_power))
 
